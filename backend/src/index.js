4d6e393d1ef243c92db41ea15ec0c72490d339b4
Samuel Angel Dzib Sosa <samuelangeldzibsosa@MacBook-Pro-de-Samuel.local>
2024-07-17 23:12:07 -0600

    remove /api

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config();
-const getAndSaveData =  require('./src/helpers/saveData');
+const getAndSaveData =  require('./helpers/saveData');
 //BD config
-require('./src/database/configs/config').dbConnection();
+require('./database/configs/config').dbConnection();
 
 //read and save data
 getAndSaveData();
@@ -12,7 +12,7 @@ getAndSaveData();
 //Middleware to JSON 
 app.use(express.json());
 
-app.use('/api/articles',require('./src/routes/article'));
+app.use('/articles',require('./routes/article'));
 
 app.listen(port, (err) => {
     if (err) throw new Error(err);
